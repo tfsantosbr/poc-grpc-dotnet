eb60c902d52111aaf1eecb3704a64726b1c8c78f
Tiago Santos <tfsantosbr@gmail.com>
2025-03-05 16:22:23 -0300

    add external service

---
+++ src/Companies.External.Api/Program.cs
@@ -0,0 +1,18 @@
+using Companies.External.Api.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapCompanyEndpoints();
+
+app.Run();
