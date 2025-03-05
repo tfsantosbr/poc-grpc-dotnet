4062f646b3c9bb41daf91023af8d2ec819cefc65
Tiago Santos <tfsantosbr@gmail.com>
2025-03-05 19:25:41 -0300

    roodou

--- src/Companies.Api/Program.cs
+++ src/Companies.Api/Program.cs
@@ -1,4 +1,6 @@
 using System.Text.Json;
+using Companies.Protos;
+using Grpc.Net.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration; ;
@@ -13,7 +15,7 @@ builder.Services.AddHttpClient("CompaniesExternalApi", client =>
 
 var app = builder.Build();
 
-app.MapGet("/companies/{cnpj}", async (string cnpj, IHttpClientFactory httpClientFactory) =>
+app.MapGet("/companies/{cnpj}/rest", async (string cnpj, IHttpClientFactory httpClientFactory) =>
 {
     var client = httpClientFactory.CreateClient("CompaniesExternalApi");
 
@@ -23,6 +25,19 @@ app.MapGet("/companies/{cnpj}", async (string cnpj, IHttpClientFactory httpClien
 
     var content = await response.Content.ReadAsStringAsync();
     var company = JsonSerializer.Deserialize<object>(content);
+
+    return Results.Json(company);
+});
+
+app.MapGet("/companies/{cnpj}/grpc", async (string cnpj, IHttpClientFactory httpClientFactory) =>
+{
+    // Configura o canal gRPC
+    using var channel = GrpcChannel.ForAddress("http://companies-external-api:8081");
+    var client = new CompanyService.CompanyServiceClient(channel);
+
+    var company = await client.GetCompanyByCnpjAsync(
+        new GetCompanyByCnpjRequest { Cnpj = cnpj }
+    );
 
     return Results.Json(company);
 });
